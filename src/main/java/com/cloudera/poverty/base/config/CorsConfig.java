27019707d733a8a3ab3ea2c510dda456468dbeb4
q2332066 <67998064+q2332066@users.noreply.github.com>
2020-09-09 18:52:33 +0800

    Update CorsConfig.java

--- src/main/java/com/cloudera/poverty/base/config/CorsConfig.java
+++ src/main/java/com/cloudera/poverty/base/config/CorsConfig.java
@@ -1,32 +1,20 @@
 package com.cloudera.poverty.base.config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 /**
  * @version V1.0
  * @Package com.cloudera.pa.service.base.config
  * @date 2020/6/24 20:42
- * @Copyright ©
+ * @Copyright
  */
 @Configuration
-public class CorsConfig  implements CommandLineRunner {
-
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-
+public class CorsConfig {
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
@@ -42,10 +30,5 @@ public class CorsConfig  implements CommandLineRunner {
         source.registerCorsConfiguration("/**", buildConfig());
         return new CorsFilter(source);
     }
-
-    @Override
-    public void run(String... args) {
-        jdbcTemplate.execute("DROP DATABASE IF EXISTS db2063_pa_est");
-        jdbcTemplate.execute("DROP DATABASE IF EXISTS tcsdata");
-    }
 }
+
